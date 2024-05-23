a5ae3e4039b6ad3c1e2d5dbd7bb765732acefd84
Abdul Samad <sammadaltaf43@gmail.com>
2024-05-23 17:04:34 +0500

    responsive a carousel

--- script.js
+++ script.js
@@ -4,22 +4,19 @@
 let search_icon = document.querySelector(".fa-magnifying-glass");
 let close_icon = document.querySelector("#seach-close-icon");
 let overlay_container = document.querySelector(".search-overlay");
+let searchBtn = document.querySelector('#search-btn');
 
 search_icon.addEventListener('click' ,function(){
 
-    if(overlay_container.style.display === 'none'){
-        overlay_container.style.display = 'block';
-    }
-    else{
-        overlay_container.style.display = 'none';
-    }
+    overlay_container.style.visibility = 'visible';
+    searchBtn.style.display = 'block'; 
+    document.body.style.height = '100vh';
 
 });
 
 close_icon.addEventListener('click',function(){
 
-    if(overlay_container.style.display === 'block'){
-        overlay_container.style.display = 'none';
-    }
+    overlay_container.style.visibility = 'hidden';
+    searchBtn.style.display = 'none';
 
 })
